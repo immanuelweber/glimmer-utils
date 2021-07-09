79981e117e82d648b08442a3f93511546243d835
Immanuel Weber <immanuel.weber@hs-koblenz.de>
2021-07-09 18:08:39 +0200

    rename module to glimmer

---
+++ glimmer/lightning/patchedlightningdatamodule.py
@@ -0,0 +1,108 @@
+import pytorch_lightning as pl
+from typing import Optional, Union, Sequence, Mapping
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+
+
+class PatchedLightningDataModule(pl.LightningDataModule):
+    @classmethod
+    def from_datasets(
+        cls,
+        train_dataset: Optional[
+            Union[Dataset, Sequence[Dataset], Mapping[str, Dataset]]
+        ] = None,
+        val_dataset: Optional[Union[Dataset, Sequence[Dataset]]] = None,
+        test_dataset: Optional[Union[Dataset, Sequence[Dataset]]] = None,
+        batch_size: int = 1,
+        num_workers: int = 0,
+        collate_fn=None,
+        persistent_workers: bool = False,
+    ):
+        r"""
+        Create an instance from torch.utils.data.Dataset.
+
+        Args:
+            train_dataset: (optional) Dataset to be used for train_dataloader()
+            val_dataset: (optional) Dataset or list of Dataset to be used for val_dataloader()
+            test_dataset: (optional) Dataset or list of Dataset to be used for test_dataloader()
+            batch_size: Batch size to use for each dataloader. Default is 1.
+            num_workers: Number of subprocesses to use for data loading. 0 means that the
+                data will be loaded in the main process. Number of CPUs available.
+
+        """
+
+        def dataloader(ds, shuffle=False, collate_fn=None, persistent_workers=False):
+            return DataLoader(
+                ds,
+                batch_size=batch_size,
+                shuffle=shuffle,
+                num_workers=num_workers,
+                pin_memory=True,
+                collate_fn=collate_fn,
+                persistent_workers=persistent_workers,
+            )
+
+        def train_dataloader():
+            if isinstance(train_dataset, Mapping):
+                return {
+                    key: dataloader(
+                        ds,
+                        shuffle=True,
+                        collate_fn=collate_fn,
+                        persistent_workers=persistent_workers,
+                    )
+                    for key, ds in train_dataset.items()
+                }
+            if isinstance(train_dataset, Sequence):
+                return [
+                    dataloader(
+                        ds,
+                        shuffle=True,
+                        collate_fn=collate_fn,
+                        persistent_workers=persistent_workers,
+                    )
+                    for ds in train_dataset
+                ]
+            return dataloader(
+                train_dataset,
+                shuffle=True,
+                collate_fn=collate_fn,
+                persistent_workers=persistent_workers,
+            )
+
+        def val_dataloader():
+            if isinstance(val_dataset, Sequence):
+                return [
+                    dataloader(
+                        ds, collate_fn=collate_fn, persistent_workers=persistent_workers
+                    )
+                    for ds in val_dataset
+                ]
+            return dataloader(
+                val_dataset,
+                collate_fn=collate_fn,
+                persistent_workers=persistent_workers,
+            )
+
+        def test_dataloader():
+            if isinstance(test_dataset, Sequence):
+                return [
+                    dataloader(
+                        ds, collate_fn=collate_fn, persistent_workers=persistent_workers
+                    )
+                    for ds in test_dataset
+                ]
+            return dataloader(
+                test_dataset,
+                collate_fn=collate_fn,
+                persistent_workers=persistent_workers,
+            )
+
+        datamodule = cls()
+        if train_dataset is not None:
+            datamodule.train_dataloader = train_dataloader
+        if val_dataset is not None:
+            datamodule.val_dataloader = val_dataloader
+        if test_dataset is not None:
+            datamodule.test_dataloader = test_dataloader
+        return datamodule
