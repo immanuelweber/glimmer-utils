8c616a1d728811c70a4c7045dc7768db096b45bf
Immanuel Weber <immanuel.weber@hs-koblenz.de>
2021-07-12 19:27:27 +0200

    init of setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name="glimmer",
+    version="0.0.1",
+    author="Immanuel Weber",
+    author_email="immanuel.weber@hs-koblenz.de",
+    description="glimmer utilities libary",
+    long_description="",
+    long_description_content_type="text/markdown",
+#     url="https://pypi.org/project/sfo/",
+    packages=setuptools.find_packages(),
+    # classifiers=[
+        # "Programming Language :: Python :: 3.7",
+        # "License :: OSI Approved :: MIT License",
+        # "Operating System :: OS Independent",
+    # ],
+)
