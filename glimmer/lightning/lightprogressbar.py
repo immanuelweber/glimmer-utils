cc455088efb49f9ab4597b98beba6c3d21f77791
Immanuel Weber <immanuel.weber@hs-koblenz.de>
2021-07-12 19:25:00 +0200

    update to recent versions

--- glimmer/lightning/lightprogressbar.py
+++ glimmer/lightning/lightprogressbar.py
@@ -1,5 +1,9 @@
 from pytorch_lightning.callbacks import ProgressBar
+from pytorch_lightning.callbacks.progress import reset, convert_inf
 from tqdm.auto import tqdm
+
+import math
+from .utils import get_max_epochs
 
 
 class LightProgressBar(ProgressBar):
@@ -7,16 +11,25 @@ class LightProgressBar(ProgressBar):
         super().__init__(refresh_rate, process_position)
 
     def init_predict_tqdm(self) -> tqdm:
-        """ Override this to customize the tqdm bar for predicting. """
+        """Override this to customize the tqdm bar for predicting."""
         bar = tqdm(disable=True)
         return bar
 
     def init_validation_tqdm(self) -> tqdm:
-        """ Override this to customize the tqdm bar for validation. """
+        """Override this to customize the tqdm bar for validation."""
         bar = tqdm(disable=True)
         return bar
 
-    def init_test_tqdm(self) -> tqdm:
-        """ Override this to customize the tqdm bar for testing. """
-        bar = tqdm(disable=True)
-        return bar
+    def on_train_start(self, trainer, pl_module):
+        super().on_train_start(trainer, pl_module)
+        self.max_epochs = math.ceil(get_max_epochs(trainer))
+
+    def on_train_epoch_start(self, trainer, pl_module):
+        super().on_train_epoch_start(trainer, pl_module)
+        self.main_progress_bar.set_description(
+            f"Epoch {trainer.current_epoch + 1}/{self.max_epochs}"
+        )
+
+    def on_test_start(self, trainer, pl_module):
+        super().on_test_start(trainer, pl_module)
+        reset(self.test_progress_bar, convert_inf(self.total_test_batches))
--- glimmer/lightning/progressprinter.py
+++ glimmer/lightning/progressprinter.py
@@ -1,3 +1,4 @@
+import random
 import time
 from typing import Any
 
@@ -36,15 +37,22 @@ class ProgressPrinter(Callback):
     # NOTE: since lightning introduced changes to the callback order on_epoch_* is useless
     # they are called prior and after each dataset cycle of train, val and test
     # this is the reason for the somehow akward use of callbacks
-    def __init__(self, highlight_best: bool = True, console: bool = False, log=None):
+    def __init__(
+        self,
+        highlight_best: bool = True,
+        console: bool = False,
+        python_logger=None,
+        silent=False,
+    ):
         self.highlight_best = highlight_best
         self.console = console
-        self.log = log
+        self.python_logger = python_logger
         self.metrics = []
-        self.best_epoch = {"loss": np.inf, "val_loss": np.inf}
+        self.best_epoch = {"loss": np.inf, "val_loss": np.inf, "epoch": -1}
         self.last_time = 0
         self.display_obj = None
         self.is_training = False
+        self.silent = silent
 
     def on_train_start(self, trainer, pl_module: LightningModule) -> None:
         self.is_training = True
@@ -57,7 +65,9 @@ class ProgressPrinter(Callback):
 
     def on_train_epoch_end(self, trainer, pl_module: LightningModule, outputs) -> None:
         if trainer.val_dataloaders is None:
-            self.report(trainer)
+            self.collect_metrics(trainer)
+            if not self.silent:
+                self.print(trainer)
 
     def on_validation_batch_start(
         self,
@@ -72,14 +82,15 @@ class ProgressPrinter(Callback):
 
     def on_validation_epoch_end(self, trainer, pl_module: LightningModule) -> None:
         if self.is_training:
-            self.report(trainer)
+            self.collect_metrics(trainer)
+            if not self.silent:
+                self.print(trainer)
 
-    def report(self, trainer) -> None:
-        raw_metrics = trainer.logged_metrics.copy()
+    def collect_metrics(self, trainer):
+        raw_metrics = trainer.callback_metrics.copy()
         metrics = {
-            "epoch": int(raw_metrics.pop("epoch")),
-            # TODO: no mean loss available for in logged metrics, better way?!
-            "loss": float(trainer.progress_bar_dict["loss"]),
+            "epoch": trainer.current_epoch,
+            "loss": float(trainer.train_loop.running_loss.mean()),
         }
 
         ignored_metrics = ["loss", "train_loss", "train_loss_epoch"]
@@ -109,6 +120,8 @@ class ProgressPrinter(Callback):
         elapsed_time = now - self.last_time
         metrics["time"] = format_time(elapsed_time)
         self.metrics.append(metrics)
+
+    def print(self, trainer) -> None:
         metrics_df = pd.DataFrame.from_records(self.metrics)
         if not self.console:
             # https://stackoverflow.com/questions/49239476/hide-a-pandas-column-while-using-style-apply
@@ -117,7 +130,8 @@ class ProgressPrinter(Callback):
                     improvement_styler, axis=None
                 ).hide_index()
             if not self.display_obj:
-                self.display_obj = display(metrics_df, display_id=42)
+                rand_id = random.randint(0, 1e6)
+                self.display_obj = display(metrics_df, display_id=42 + rand_id)
             else:
                 self.display_obj.update(metrics_df)
         else:
@@ -133,14 +147,20 @@ class ProgressPrinter(Callback):
                 [f"{key}: {val}" for key, val in metrics.items() if key != "epoch"]
             )
             pad = len(str(trainer.max_epochs))
-            if self.log:
-                self.log.info(f"{last_row.name:>{pad}}/{trainer.max_epochs}: {metrics}")
+            if self.python_logger:
+                self.python_logger.info(
+                    f"{last_row.name:>{pad}}/{trainer.max_epochs}: {metrics}"
+                )
             else:
                 print(f"{last_row.name:>{pad}}/{trainer.max_epochs}: {metrics}")
 
-    def static_report(self, verbose: bool = True) -> pd.DataFrame:
-        metrics_df = pd.DataFrame.from_records([self.best_epoch, self.metrics[-1]])
+    def static_print(self, verbose: bool = True) -> pd.DataFrame:
+        if len(self.metrics):
+            last_epoch = self.metrics[-1]
+        else:
+            last_epoch = self.best_epoch
+        metrics_df = pd.DataFrame.from_records([self.best_epoch, last_epoch])
         metrics_df.index = ["best", "last"]
         if verbose:
-            display(metrics_df, display_id=43)
+            display(metrics_df, display_id=43 + random.randint(0, 1e6))
         return metrics_df
