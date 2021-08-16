bff946b88c92ef7a867912c739b91e3c7e214860
Immanuel Weber <immanuel.weber@gmail.com>
2021-08-16 12:01:03 +0200

    fix static printing by seperating metric collection from printing

--- glimmer/lightning/progressprinter.py
+++ glimmer/lightning/progressprinter.py
@@ -55,26 +55,28 @@ class ProgressPrinter(Callback):
         self.last_time = time.time()
 
     def on_train_epoch_end(self, trainer, pl_module: LightningModule, outputs) -> None:
+        self.collect_metrics(trainer)
         if not self.silent:
             self.print(trainer)
 
-    def print(self, trainer) -> None:
-        raw_metrics = trainer.logged_metrics.copy()
+    def collect_metrics(self, trainer):
         metrics = {
             "epoch": trainer.current_epoch,
             # TODO: no mean loss available for in logged metrics, better way?!
             "loss": float(trainer.progress_bar_dict["loss"]),
         }
 
-        ignored_metrics = ["loss", "train_loss", "train_loss_epoch", "epoch"]
+        raw_metrics = trainer.logged_metrics.copy()
+        ignored_metrics = ["loss", "epoch"]
         for m in ignored_metrics:
             if m in raw_metrics:
                 raw_metrics.pop(m)
-
         if "val_loss" in raw_metrics:
             metrics["val_loss"] = float(raw_metrics.pop("val_loss"))
         elif "val_loss_epoch" in raw_metrics:
             metrics["val_loss"] = float(raw_metrics.pop("val_loss_epoch"))
+        if "val_loss_step" in raw_metrics:
+            raw_metrics.pop("val_loss_step")
         for key, value in raw_metrics.items():
             if key.endswith("_epoch"):
                 metrics[key[6:]] = float(value)
@@ -92,13 +94,14 @@ class ProgressPrinter(Callback):
         elapsed_time = now - self.last_time
         metrics["time"] = format_time(elapsed_time)
         self.metrics.append(metrics)
+
+    def print(self, trainer) -> None:
         metrics_df = pd.DataFrame.from_records(self.metrics)
         if not self.console:
             # https://stackoverflow.com/questions/49239476/hide-a-pandas-column-while-using-style-apply
             if self.highlight_best:
-                metrics_df = metrics_df.style.apply(
-                    improvement_styler, axis=None
-                ).hide_index()
+                metrics_df = metrics_df.style.apply(improvement_styler, axis=None)
+                metrics_df = metrics_df.hide_index()
             if not self.display_obj:
                 rand_id = random.randint(0, 1e6)
                 self.display_obj = display(metrics_df, display_id=42 + rand_id)
@@ -108,11 +111,11 @@ class ProgressPrinter(Callback):
             last_row = metrics_df.iloc[-1]
 
             metrics = {index: last_row[index] for index in last_row.index}
-            metrics = {
-                key: f"{val:.4f}" if isinstance(val, float) else val
-                for key, val in metrics.items()
-            }
 
+            def __format(val):
+                return f"{val:.4f}" if isinstance(val, float) else val
+
+            metrics = {key: __format(val) for key, val in metrics.items()}
             metrics = ", ".join(
                 [f"{key}: {val}" for key, val in metrics.items() if key != "epoch"]
             )
@@ -125,7 +128,12 @@ class ProgressPrinter(Callback):
                 print(f"{last_row.name:>{pad}}/{trainer.max_epochs}: {metrics}")
 
     def static_print(self, verbose: bool = True) -> pd.DataFrame:
-        metrics_df = pd.DataFrame.from_records([self.best_epoch, self.metrics[-1] if len(self.metrics) else self.best_epoch])
+        metrics_df = pd.DataFrame.from_records(
+            [
+                self.best_epoch,
+                self.metrics[-1] if len(self.metrics) else self.best_epoch,
+            ]
+        )
         metrics_df.index = ["best", "last"]
         if verbose:
             display(metrics_df, display_id=43 + random.randint(0, 1e6))
