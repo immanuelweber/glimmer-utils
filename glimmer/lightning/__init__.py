7f6da8fcd4216cb67a9b2d79323ccd74ce945884
immanuelweber <immanuel.weber@gmail.com>
2025-06-17 14:27:29 +0200

    Export lightning utilities

--- glimmer/lightning/__init__.py
+++ glimmer/lightning/__init__.py
@@ -1,5 +1,11 @@
 """Utilities for PyTorch Lightning."""
 
+# re-export frequently used symbols for convenience
+from .lightprogressbar import LightProgressBar
+from .progressprinter import ProgressPrinter
+from .progressplotter import ProgressPlotter
+from .patcheddatamodule import PatchedDataModule
+
 __all__ = [
     "LightProgressBar",
     "ProgressPrinter",
